75683a47b75914eda6e11827c2c3eb49399afed9
Minsi Yang <yangminsi@gmail.com>
2020-09-16 14:58:56 +0100

    Fix tests for TodoList component

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import TodoList from './TodoList';
 
-test('user can add a todo note', () => {
+test('it can render a todo note', () => {
   render(<TodoList lists={["Get Milk"]} />)
   const todo = screen.getByText("Get Milk")
+  const checkBox = screen.getByRole("checkbox")
   expect(todo).toBeInTheDocument();
+  expect(checkBox).toBeInTheDocument();
 })
 
-// test('user can check off a list', () => {
-//   render(<TodoList />)
-//   // const textBox = screen.getByLabelText("Add Todo")
-//   // userEvent.type(textBox, "Get Milk")
-//   // userEvent.click(screen.getByText(/save/i))
-
-//   const checkBox = screen.getByRole("checkbox")
-//   userEvent.click(checkBox)
-//   expect(checkBox).toBeChecked();
-// })
-
-// test('user can delete the whole list', () => {
-//   render(<TodoList />)
-//   const textBox = screen.getByRole("textbox")
-//   userEvent.type(textBox, "School Pickup")
-//   userEvent.click(screen.getByText(/save/i))
-//   const todo = screen.getByText(/school pickup/i)
-//   expect(todo).toBeInTheDocument()
-//   userEvent.click(screen.getByText(/reset/i))
-//   expect(todo).not.toBeInTheDocument();
-// })
+test('user can check off lists', () => {
+  render(<TodoList lists={["Get Milk", "School Pickup"]}/>)
+  const todoOne = screen.getByText("Get Milk")
+  const todoTwo = screen.getByText("School Pickup")
+  expect(todoOne).toBeInTheDocument();
+  expect(todoTwo).toBeInTheDocument();
+})
